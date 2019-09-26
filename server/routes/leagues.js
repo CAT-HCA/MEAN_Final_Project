04eff12978d35d111a021475f64df874494335f1
Cori <corinne.a.ader@gmail.com>
2019-09-26 14:47:44 -0400

    server cleaned up

--- server/routes/leagues.js
+++ server/routes/leagues.js
@@ -3,7 +3,6 @@ const api = require('../controllers/leagues');
 var router = express.Router();
 
 
-
 // GET http://localhost:3000/leagues/data
 router.get("/data", api.getLeaguesData);
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,26 @@
 var express = require("express");
-const api = require('../controllers/user');
+const api = require("../controllers/user");
 var router = express.Router();
-
 
 // GET http://localhost:3000/users/data/{id}
 router.get("/data/", api.getAllData);
 
 // GET http://localhost:3000/users/data/{id}
-router.get("/data/:user_id", api.getData);
-
-// POST http://localhost:3000/users/login
-router.post("/login/", api.login);
+router.get("/data/:id", api.getData);
 
 // POST http://localhost:3000/users/register
 router.post("/register/", api.register);
 
-// PUT http://localhost:3000/users/update
-router.put("/update/", api.update);
+// POST http://localhost:3000/users/login
+router.post("/login/", api.login);
+
+// PUT http://localhost:3000/users/update/{id}
+router.put("/update/:id", api.update);
 
 // DELETE: http://localhost:3000/delete/{id}
-router.delete('/delete/:user_id', api.delete);
+router.delete("/delete/:id", api.delete);
 
 // GET: http://localhost:3000/users/logout
-router.get('/logout', api.logOut);
+router.get("/logout", api.logOut);
 
 module.exports = router;
